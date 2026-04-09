d567b679061e8011bff9d091bb9879edb1133a79
hetasshi <artemplot073@gmail.com>
2026-04-09 15:39:13 +0300

    feat: implement custom respawn location logic for match worlds in RespawnListener

--- src/main/java/me/matistan05/minecraftmanhunt/listeners/RespawnListener.java
+++ src/main/java/me/matistan05/minecraftmanhunt/listeners/RespawnListener.java
@@ -2,6 +2,8 @@ package me.matistan05.minecraftmanhunt.listeners;
 
 import me.matistan05.minecraftmanhunt.Main;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerRespawnEvent;
@@ -9,16 +11,47 @@ import org.bukkit.event.player.PlayerRespawnEvent;
 import static me.matistan05.minecraftmanhunt.commands.ManhuntCommand.*;
 
 public class RespawnListener implements Listener {
+    private final Main main;
+
     public RespawnListener(Main main) {
+        this.main = main;
         Bukkit.getPluginManager().registerEvents(this, main);
     }
 
     @EventHandler
     public void RespawnEvent(PlayerRespawnEvent e) {
-        if ((inGame || waitingForStart) && isHunter(e.getPlayer().getName())) {
+        if (!(inGame || waitingForStart) || !isInGame(e.getPlayer().getName())) {
+            return;
+        }
+
+        Location respawnLocation = resolveMatchRespawnLocation();
+        if (respawnLocation != null) {
+            e.setRespawnLocation(respawnLocation);
+        }
+
+        if (isHunter(e.getPlayer().getName())) {
             if (waypointManager.getCompassSlot(e.getPlayer()) == -1) {
                 e.getPlayer().getInventory().setItem(8, waypointManager.getCompass());
             }
         }
+    }
+
+    private Location resolveMatchRespawnLocation() {
+        if (startLocation != null) {
+            return startLocation.clone();
+        }
+
+        if (!main.getConfig().getBoolean("matchWorlds.enabled", false)) {
+            return null;
+        }
+
+        for (String worldName : main.getMatchWorldManager().getActiveWorldNames()) {
+            World world = Bukkit.getWorld(worldName);
+            if (world != null && world.getEnvironment() == World.Environment.NORMAL) {
+                return world.getSpawnLocation();
+            }
+        }
+
+        return null;
     }
 }
